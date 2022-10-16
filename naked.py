a3690e133987fbce30f6fbfab635ca62bc2d8d20
ilauga <ievalauga@gmail.com>
2022-10-16 15:44:43 +0000

    Ierakstita NASA API atslega

--- naked.py
+++ naked.py
@@ -11,7 +11,7 @@ print('Asteroid processing service')
 print('Loading configuration from file')
 
 # 
-nasa_api_key = "???"
+nasa_api_key = "dQ9TVsaiOTDMRE6q1zA3i1UaiEChoW2jpWUzQ9HK"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
