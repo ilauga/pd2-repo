02afeaba1f9c6186edac452bef802d4bd8f37458
ilauga <ievalauga@gmail.com>
2022-10-16 17:43:11 +0000

    Pievienots external config fails

--- naked.py
+++ naked.py
@@ -5,14 +5,24 @@ import time
 import yaml
 
 from datetime import datetime
+from configparser import ConfigParser
+
 print('Asteroid processing service')
 
 # Initiating and reading config values
 print('Loading configuration from file')
 
-# Setting NASA url  & key
-nasa_api_key = "dQ9TVsaiOTDMRE6q1zA3i1UaiEChoW2jpWUzQ9HK"
-nasa_api_url = "https://api.nasa.gov/neo/"
+
+try:
+	config = ConfigParser()
+	config.read('config.ini')
+
+	nasa_api_key = config.get('nasa', 'api_key')
+	nasa_api_url = config.get('nasa', 'api_url')
+
+except:
+	logger.exception('')
+print('DONE')
 
 # Getting todays date
 dt = datetime.now()
